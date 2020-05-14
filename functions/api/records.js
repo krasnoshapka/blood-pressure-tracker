23ae54d291a1f78eb798a6fc4d8e19a2e5cf0546
krasnoshapka <shlapa@gmail.com>
2020-05-14 15:53:10 +0300

    Inital records api implementation.

--- functions/api/records.js
+++ functions/api/records.js
@@ -1,21 +1,26 @@
+const { db } = require('../util/admin');
+
 exports.getAllRecords = (request, response) => {
-  records = [
-    {
-      'id': 1,
-      'user': 1,
-      'datetime': new Date().toJSON(),
-      'sys': 150,
-      'dia': 100,
-      'pul': 80
-    },
-    {
-      'id': 2,
-      'user': 1,
-      'datetime': new Date().toJSON(),
-      'sys': 130,
-      'dia': 90,
-      'pul': 75
-    }
-  ]
-  return response.json(records);
-}
+  db
+    .collection('records')
+    .orderBy('datetime', 'desc')
+    .get()
+    .then((data) => {
+      let records = [];
+      data.forEach((doc) => {
+        records.push({
+          id: doc.id,
+          user: doc.data().user,
+          datetime: doc.data().datetime,
+          sys: doc.data().sys,
+          dia: doc.data().dia,
+          pul: doc.data().pul
+        });
+      });
+      return response.json(records);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code});
+    });
+};
