0c81e2350c4ede8f6561f2af44e9f3c322aad451
krasnoshapka <shlapa@gmail.com>
2020-06-05 16:18:10 +0300

    Converted Home component from class to function using hooks.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import BottomNavigation from '@material-ui/core/BottomNavigation';
@@ -41,74 +41,66 @@ const styles = (theme) => ({
   },
 });
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
+const HomePage = (props) => {
+  const [settings, setSettings] = useState({email: ''});
+  const [uiLoading, setUiLoading] = useState(true);
+  const [page, setPage] = useState('pressure');
+  const [errorMsg, setErrorMsg] = useState('');
 
-    this.state = {
-      uiLoading: true,
-      page: 'pressure'
-    };
-  }
-
-  handlePageChange = (event, newPage) => {
+  const handlePageChange = (event, newPage) => {
     if (newPage === 'logout') {
       localStorage.removeItem('AuthToken');
-      this.props.history.push(`${HOME_ROUTE}/login`);
+      props.history.push(`${HOME_ROUTE}/login`);
     }
-    this.setState({ page: newPage });
+    setPage(newPage);
   }
 
-  componentWillMount = () => {
-    authMiddleWare(this.props.history);
+  useEffect(() => {
+    authMiddleWare(props.history);
     const authToken = localStorage.getItem('AuthToken');
     axios.defaults.headers.common = { Authorization: `${authToken}` };
     axios
       .get(`${API_ROUTE}/user/`)
       .then((response) => {
-        this.setState({
-          email: response.data.email,
-          uiLoading: false,
-        });
+        setSettings(response.data);
+        setUiLoading(false)
       })
       .catch((error) => {
         if (error.response.status === 403) {
-          this.props.history.push(`${HOME_ROUTE}/login`)
+          props.history.push(`${HOME_ROUTE}/login`)
         }
         console.log(error);
-        this.setState({ errorMsg: 'Error in retrieving the data' });
+        setErrorMsg('Error in retrieving the data');
       });
-  };
+  }, []);
 
-  render() {
-    const { classes } = this.props;
-    if (this.state.uiLoading === true) {
-      return (
-        <div className={classes.root}>
-          {this.state.uiLoading && <CircularProgress size={150} className={classes.uiProgess} />}
+  const { classes } = props;
+  if (uiLoading === true) {
+    return (
+      <div className={classes.root}>
+        {uiLoading && <CircularProgress size={150} className={classes.uiProgess} />}
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.root}>
+        <div className={classes.content}>
+          {(() => {
+            switch (page) {
+              case 'pressure': return <Records />;
+              case 'add': return <AddRecord />;
+              case 'settings': return <Settings />;
+            }
+          })()}
         </div>
-      );
-    } else {
-      return (
-        <div className={classes.root}>
-          <div className={classes.content}>
-            {(() => {
-              switch (this.state.page) {
-                case 'pressure': return <Records />;
-                case 'add': return <AddRecord />;
-                case 'settings': return <Settings />;
-              }
-            })()}
-          </div>
-          <BottomNavigation value={this.state.page} onChange={this.handlePageChange} className={classes.bottomMenu}>
-            <BottomNavigationAction label="Pressure" value="pressure" icon={<NotesIcon />} />
-            <BottomNavigationAction label="Add pressure" value="add" icon={<AddCircleIcon />} />
-            <BottomNavigationAction label="Settings" value="settings" icon={<AccountBoxIcon />} />
-            <BottomNavigationAction label="Logout" value="logout" icon={<ExitToAppIcon />} />
-          </BottomNavigation>
-        </div>
-      );
-    }
+        <BottomNavigation value={page} onChange={handlePageChange} className={classes.bottomMenu}>
+          <BottomNavigationAction label="Pressure" value="pressure" icon={<NotesIcon />} />
+          <BottomNavigationAction label="Add pressure" value="add" icon={<AddCircleIcon />} />
+          <BottomNavigationAction label="Settings" value="settings" icon={<AccountBoxIcon />} />
+          <BottomNavigationAction label="Logout" value="logout" icon={<ExitToAppIcon />} />
+        </BottomNavigation>
+      </div>
+    );
   }
 }
 
