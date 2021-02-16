39ac727eb1bb774321ab76933b662a40c02c1645
krasnoshapka <shlapa@gmail.com>
2021-02-16 12:01:24 +0200

    Refactored Signup styles.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,7 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -15,24 +14,23 @@ import axios from 'axios';
 import {HOME_ROUTE, API_ROUTE} from "../constants/routes";
 
 const styles = (theme) => ({
-  paper: {
+  container: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    margin: theme.spacing(2),
+    backgroundColor: theme.palette.primary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(2)
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
   },
-  progess: {
+  progress: {
     position: 'absolute'
   }
 });
@@ -70,83 +68,74 @@ const SignupPage = (props) => {
 
   const {classes} = props;
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                helperText={errors.email}
-                error={errors.email ? true : false}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                helperText={errors.password}
-                error={errors.password ? true : false}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="confirmPassword"
-                label="Confirm Password"
-                type="password"
-                id="confirmPassword"
-                autoComplete="current-password"
-                helperText={errors.confirmPassword}
-                error={errors.confirmPassword ? true : false}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleSubmit}
-            disabled={loading ||
-            !userData.email ||
-            !userData.password}
-          >
-            Sign Up
-            {loading && <CircularProgress size={30} className={classes.progess} />}
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="login" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+    <Container component="main" maxWidth="xs" className={classes.container}>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign up
+      </Typography>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          helperText={errors.email}
+          error={errors.email ? true : false}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          helperText={errors.password}
+          error={errors.password ? true : false}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="confirmPassword"
+          label="Confirm Password"
+          type="password"
+          id="confirmPassword"
+          autoComplete="current-password"
+          helperText={errors.confirmPassword}
+          error={errors.confirmPassword ? true : false}
+          onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick={handleSubmit}
+          disabled={loading ||
+          !userData.email ||
+          !userData.password}
+        >
+          Sign Up
+          {loading && <CircularProgress size={30} className={classes.progress} />}
+        </Button>
+      </form>
+      <div>
+        <Link href="login" variant="body2">
+          Already have an account? Sign in
+        </Link>
       </div>
     </Container>
   );
