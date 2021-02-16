1f86e55a0d2fedf77a0da2233a384f93b4a404ca
krasnoshapka <shlapa@gmail.com>
2021-02-16 11:27:36 +0200

    Refactored Header styles.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import withStyles from "@material-ui/core/styles/withStyles";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -14,16 +13,13 @@ const styles = (theme) => ({
 const Header = (props) => {
   const {classes} = props;
   return (
-    <React.Fragment>
-      <CssBaseline/>
-      <AppBar position="fixed" className={classes.topBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Blood Pressure Tracker
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
+    <AppBar position="fixed" className={classes.topBar}>
+      <Toolbar>
+        <Typography variant="h4" noWrap>
+          Blood Pressure Tracker
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
