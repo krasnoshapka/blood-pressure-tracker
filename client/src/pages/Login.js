6ab4fb9896321ed12b6845cc4885d9e481d8cbda
krasnoshapka <shlapa@gmail.com>
2021-02-16 11:50:42 +0200

    Refactored Login styles.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -15,19 +14,18 @@ import axios from 'axios';
 import {HOME_ROUTE, API_ROUTE} from '../constants/routes';
 
 const styles = (theme) => ({
-  paper: {
+  container: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    margin: theme.spacing(2),
+    backgroundColor: theme.palette.primary.main
   },
   form: {
-    width: '100%',
-    marginTop: theme.spacing(1)
+    marginTop: theme.spacing(2)
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
@@ -35,9 +33,9 @@ const styles = (theme) => ({
   customError: {
     color: 'red',
     fontSize: '0.8rem',
-    marginTop: 10
+    display: 'inline-block'
   },
-  progess: {
+  progress: {
     position: 'absolute'
   }
 });
@@ -74,68 +72,65 @@ const LoginPage = (props) => {
 
   const { classes } = props;
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            helperText={errors.email}
-            error={errors.email ? true : false}
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            helperText={errors.password}
-            error={errors.password ? true : false}
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleSubmit}
-            disabled={loading || !userData.email || !userData.password}
-          >
-            Sign In
-            {loading && <CircularProgress size={30} className={classes.progess} />}
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-          {errors.general && (
-            <Typography variant="body2" className={classes.customError}>
-              {errors.general}
-            </Typography>
-          )}
-        </form>
+    <Container component="main" maxWidth="xs" className={classes.container}>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
+      <form className={classes.form} noValidate>
+        {errors.general && (
+          <Typography variant="body2" className={classes.customError}>
+            {errors.general}
+          </Typography>
+        )}
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          helperText={errors.email}
+          error={errors.email ? true : false}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          helperText={errors.password}
+          error={errors.password ? true : false}
+          onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick={handleSubmit}
+          disabled={loading || !userData.email || !userData.password}
+        >
+          Sign In
+          {loading && <CircularProgress size={30} className={classes.progress} />}
+        </Button>
+      </form>
+
+      <div>
+        <Link href="signup" variant="body2">
+          {"Don't have an account? Sign Up"}
+        </Link>
       </div>
     </Container>
   );
